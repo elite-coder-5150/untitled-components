d9ce267707110999ddcb4f36a66bff9f7c14d6fb
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-05-08 23:14:07 -0500

    added the console log statement to tell the user that they have successfully logged in.

--- client/typescript/app.ts
+++ client/typescript/app.ts
@@ -1,11 +1,23 @@
 import * as $ from "jquery";
+
+import { SubscriptionForm } from "./subscriber";
 class App {
   newsletterForm: HTMLFormElement;
+  subscriptionForm: HTMLFormElement;
+  registerForm: HTMLFormElement;
+  loginForm: HTMLFormElement;
   notification: HTMLElement;
+  sform: HTMLFormElement;
+  subscriptionList: HTMLUListElement;
 
   constructor() {
     this.newsletterForm = new HTMLFormElement();
+    this.subscriptionForm = new HTMLFormElement();
     this.notification = new HTMLElement();
+    this.subscriptionList = new HTMLUListElement();
+    this.registerForm = new HTMLFormElement();
+    this.loginForm = new HTMLFormElement();
+ 
     this.initEvents();
   }
 
@@ -13,13 +25,27 @@ class App {
     this.newsletterForm = document.querySelector(
       ".newsletter-form"
     ) as HTMLFormElement;
-    //this.notification = document.querySelector('.notification');
+    this.subscriptionForm = document.querySelector(
+        '.subscription-form'
+    ) as HTMLFormElement;
+
+    this.registerForm = document.querySelector(
+        '.register-form'
+    ) as HTMLFormElement;
+
+    this.loginForm = document.querySelector(
+        '.login-form'
+    ) as HTMLFormElement;
+    
   }
 
   initEvents() {
     this.newsletterForm.addEventListener("submit", this.onSubmitNewsletter);
 
     this.notification.addEventListener("click", this.toggleNotification);
+    this.subscriptionForm.addEventListener("submit", this.onSubmitSubscription);
+    this.registerForm.addEventListener("submit", this.onRegisterSubmit);
+    this.loginForm.addEventListener("submit", this.onLoginSubmit);
   }
 
   toggleNotification() {
@@ -36,10 +62,44 @@ class App {
     event.preventDefault();
     const input = this.newsletterForm.querySelector("input");
     // const email = input?.value;
-
-    if (input) {
-      const email = input.value;
+    const subscriber = input?.value
+    if (subscriber) {
+      const subscriber = input.value;
       input.value = "";
     }
+
+
+  }
+
+  onSubmitSubscription(e: Event) {
+    e.preventDefault();
+
+    //this.updateSubscriberList(this.subscriberList);
+
+  }
+
+  updateSubscriberList(subscriber: string) {
+    const subscriberList = document.querySelectorAll('.subscriber-list');
+
+    if (subscriberList instanceof HTMLUListElement) {
+        const newSubItem = document.createElement('li');
+        newSubItem.textContent = subscriber;
+        subscriberList.appendChild(newSubItem);
+    }
+  }
+
+  sendNewSubNotification(subscriber: string): void {
+    console.log(`New Subscriber: ${subscriber}`);
+  }
+
+  onRegisterSubmit(e: Event): void {
+    e.preventDefault();
+  }
+
+  onLoginSubmit(e: Event) {
+    e.preventDefault();
+
+    console.log('you are logged in');
+
   }
 }
